0dd5042907e9dfd657ced7cb3de469bc82e24025
Kayla-PGIP <168926178+Kayla-PGIP@users.noreply.github.com>
2024-05-11 15:44:53 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Random;
+
+namespace Hangman_2_
+{
+    internal class Program
+    {
+
+        private static void printHangman(int wrong)
+        {
+            if(wrong == 0)
+            {
+                Console.WriteLine("\n+---+");
+                Console.WriteLine("     |");
+                Console.WriteLine("     |");
+                Console.WriteLine("     |");
+                Console.WriteLine("    ===");
+
+            }
+            else if(wrong == 1)
+            {
+                Console.WriteLine("\n+---+");
+                Console.WriteLine("O     |");
+                Console.WriteLine("      |");
+                Console.WriteLine("      |");
+                Console.WriteLine("    ===");
+            }
+            else if (wrong == 2)
+            {
+                Console.WriteLine("\n+---+");
+                Console.WriteLine("O     |");
+                Console.WriteLine("|     |");
+                Console.WriteLine("      |");
+                Console.WriteLine("    ===");
+            }
+            else if (wrong == 3)
+            {
+                Console.WriteLine("\n+---+");
+                Console.WriteLine(" O    |");
+                Console.WriteLine("/|    |");
+                Console.WriteLine("      |");
+                Console.WriteLine("    ===");
+            }
+            else if (wrong == 4)
+            {
+                Console.WriteLine("\n+---+");
+                Console.WriteLine(" O    |");
+                Console.WriteLine("/|\\  |");
+                Console.WriteLine("      |");
+                Console.WriteLine("    ===");
+            }
+            else if (wrong == 5)
+            {
+                Console.WriteLine("\n+---+");
+                Console.WriteLine(" O    |");
+                Console.WriteLine("/|\\  |");
+                Console.WriteLine("/     |");
+                Console.WriteLine("    ===");
+            }
+            else if (wrong == 6)
+            {
+                Console.WriteLine("\n+----+");
+                Console.WriteLine(" O    |");
+                Console.WriteLine("/|\\  |");
+                Console.WriteLine("/ \\  |");
+                Console.WriteLine("     ===");
+            }
+            
+
+        }
+
+        private static int printWord(List<char>guessedLetters, String randomWord)
+        {
+            int counter = 0;
+            int rightLetters = 0;
+            Console.WriteLine("\r\n");
+            foreach (char c in randomWord)
+            {
+                if(guessedLetters.Contains(c))
+                {
+                    Console.WriteLine(c + " ");
+                    rightLetters++;
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+                counter++;
+            }
+            return rightLetters;
+        }
+
+        private static void printLines(String randomWord)
+        {
+            Console.Write("\r");
+            foreach (char c in randomWord)
+            {
+                Console.OutputEncoding = System.Text.Encoding.Unicode;
+                Console.Write("\u0305");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Hangman :)");
+            Console.WriteLine("--------------------------");
+
+            Random random = new Random();
+            List<string> wordDictionary = new List<string> { "diamond", "lion", "book", "sandwhich", "freezer", "window", "keyboard", "blanket", "family tree", "dance", "computer", "charger"};
+            int index = random.Next(wordDictionary.Count);
+            String randomWord = wordDictionary[index];
+
+            foreach (char x  in randomWord)
+            {
+                Console.WriteLine("_ ");
+            }
+
+            int lengthOfWordToGuess = randomWord.Length;
+            int amountOfTimesWrong = 0;
+            List<char> currentLettersGuessed = new List<char>();
+            int currentLetterRight = 0;
+
+            while(amountOfTimesWrong != 6 && currentLetterRight != lengthOfWordToGuess)
+            {
+                Console.Write("\nLetters guessed so far: ");
+                foreach(char letter in currentLettersGuessed)
+                {
+                    Console.Write(letter + " ");
+                }
+
+                Console.Write("\nGuess a letter: ");
+                char letterGuessed = Console.ReadLine()[0];
+
+                if(currentLettersGuessed.Contains(letterGuessed))
+                {
+                    Console.Write("\r\nYou have already guessed this letter.");
+                    printHangman(amountOfTimesWrong);
+                    currentLetterRight = printWord(currentLettersGuessed, randomWord);
+                    printLines(randomWord);
+                }
+                else
+                {
+                    bool right = false;
+                    for (int i = 0; i < randomWord.Length; i++) { if (letterGuessed == randomWord[i]) { right = true;} }
+
+                    if (right)
+                    {
+                        printHangman(amountOfTimesWrong);
+                        currentLettersGuessed.Add(letterGuessed);
+                        currentLetterRight = printWord(currentLettersGuessed, randomWord);
+                        Console.Write("\r\n");
+                        printLines(randomWord);
+                    }
+                    else
+                    {
+                        amountOfTimesWrong++;
+                        currentLettersGuessed.Add(letterGuessed);
+                        printHangman(amountOfTimesWrong);
+                        currentLetterRight = printWord(currentLettersGuessed, randomWord);
+                        Console.Write("\r\n");
+                        printLines(randomWord);
+
+                    }
+                }
+            }
+            Console.WriteLine("\r\nGame is over! Thanks for playing!");
+        }
+    }
+}
